f67df4988c8032b3e5c9de86023065c47b62519f
Chris Gomez <christiangomezdev@gmail.com>
2023-03-28 02:01:09 -0400

    Improving carousel

--- src/app/components/product-list/product-item.tsx/product-item.tsx
+++ src/app/components/product-list/product-item.tsx/product-item.tsx
@@ -1,5 +1,5 @@
 import { Product } from "@prisma/client";
-import { CSSProperties } from "react";
+import React, { CSSProperties, Ref } from "react";
 import styles from "./product-item.module.css";
 
 type IProductItemProps = {
@@ -8,24 +8,29 @@ type IProductItemProps = {
   className?: string;
 };
 
-export const ProductItem = ({
-  product,
-  style,
-  className,
-}: IProductItemProps) => {
-  return (
-    <div className={`${styles.product_item} ${className}`} style={style}>
-      <img
-        className={styles.product_image}
-        src={"https://placehold.it/200x200"}
-        alt="product image"
-      />
-      <p className={styles.product_name}>Name</p>
-      <p className={styles.product_price}>12.0</p>
-      <p className={styles.product_description}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        cursus eu sem ac commodo. Integer porttitor ullamcorper tristique.
-      </p>
-    </div>
-  );
-};
+export const ProductItem = React.forwardRef(
+  (
+    { product, style, className }: IProductItemProps,
+    ref: Ref<HTMLDivElement>
+  ) => {
+    return (
+      <div
+        className={`${styles.product_item} ${className}`}
+        style={style}
+        ref={ref}
+      >
+        <img
+          className={styles.product_image}
+          src={"https://placehold.it/200x200"}
+          alt="product image"
+        />
+        <p className={styles.product_name}>Name</p>
+        <p className={styles.product_price}>12.0</p>
+        <p className={styles.product_description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          cursus eu sem ac commodo. Integer porttitor ullamcorper tristique.
+        </p>
+      </div>
+    );
+  }
+);
--- src/app/components/product-list/product-list.tsx
+++ src/app/components/product-list/product-list.tsx
@@ -3,7 +3,7 @@
 import { Product } from "@prisma/client";
 import styles from "./product-list.module.css";
 import { ProductItem } from "./product-item.tsx/product-item";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useWindowDimensions } from "@components/app/hooks/dimensions";
 
 type IProductListProps = {
@@ -13,11 +13,31 @@ type IProductListProps = {
 export const ProductList = ({ products }: IProductListProps) => {
   const [offset, setOffset] = useState(0);
 
-  const { width, height } = useWindowDimensions();
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const itemRef = useRef<HTMLDivElement>(null);
+
+  const [firstItem, setFirstItem] = useState<Product>();
+  const [productList, setProductList] = useState<Product[]>();
+
+  useEffect(() => {
+    if (products.length > 0) {
+      setFirstItem(products[0]);
+      setProductList(products.slice(1, -1));
+    }
+  }, []);
+
+  const getItemWidth = () => {
+    return itemRef.current == null ? 0 : itemRef.current.clientWidth;
+  };
 
   const updateOffset = (newOffset: number) => {
     let offset = newOffset;
-    if ((newOffset + (250 * 2)) > width) {
+    if (
+      containerRef.current != null &&
+      newOffset - getItemWidth() * (products.length - products.length / 2) >
+        containerRef.current?.clientWidth
+    ) {
       offset = 0;
     }
     setOffset(offset);
@@ -25,8 +45,8 @@ export const ProductList = ({ products }: IProductListProps) => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      updateOffset(offset + 250);
-    }, 1000);
+      updateOffset(offset + getItemWidth() + 16);
+    }, 1600);
 
     return () => {
       if (interval) {
@@ -36,19 +56,29 @@ export const ProductList = ({ products }: IProductListProps) => {
   });
 
   const getItemPosition = (index: number) => {
-    return -offset;
+    return offset * -1;
   };
 
   return (
-    <div className={styles.product_list}>
-      {products &&
-        products.length > 0 &&
-        products.map((product, index) => (
+    <div className={styles.product_list} ref={containerRef}>
+      {firstItem && (
+        <ProductItem
+          ref={itemRef}
+          key={firstItem.id}
+          product={firstItem}
+          style={{
+            transform: "translateX(" + getItemPosition(0) + "px)",
+          }}
+        />
+      )}
+      {productList &&
+        productList.length > 0 &&
+        productList.map((product, index) => (
           <ProductItem
             key={product.id}
             product={product}
             style={{
-              transform: "translateX(" + getItemPosition(index) + "px)",
+              transform: "translateX(" + getItemPosition(index + 1) + "px)",
             }}
           />
         ))}
