0ba44a7067d468b409816c8caf97d594a0837d26
Chris Gomez <christiangomezdev@gmail.com>
2023-03-29 00:42:03 -0400

    Improving docker

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+
+npx prisma generate
+
+yarn build
 npx prisma migrate deploy
 npx prisma db seed
 yarn start
--- src/app/utils/urlUtil.ts
+++ src/app/utils/urlUtil.ts
@@ -1,9 +1,11 @@
 const publicUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
+
 const getFullPath = (path: string) => {
   if (path.startsWith("http" || path.startsWith("https"))) {
     return path;
   }
+  console.log({publicUrl});
   return publicUrl + path;
 };
 
